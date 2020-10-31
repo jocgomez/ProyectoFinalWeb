7e753a84fe277720b03316cd93819364928310f3
Kevin Marmolejo <kevin.marmolejo@uao.edu.co>
2020-10-31 18:40:38 -0500

    Boton con icono de red social

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import "./Button.css"
+import "./Button.css";
+import GmailIcon from '../../Images/Icons/Gmail.png'; 
+import FacebookIcon from '../../Images/Icons/Facebook.png';
+import PhoneIcon from '../../Images/Icons/Phone.png';
 
 const colors = {
     Blue: '#1A1446',
@@ -8,22 +11,42 @@ const colors = {
     Black: '#333333,',
 }
 
-function GetColor(color){
+const icons = {
+    Gmail: GmailIcon,
+    Facebook: FacebookIcon,
+    Phone: PhoneIcon,
+}
+
+function GetColor(color) {
     let colorCss = colors[color] ? colors[color] : "#1A1446";
     return colorCss;
 }
 
-function Button({colorBackground, colorText, text, onClick}) {
+function GetIcon(icon) {
+    let iconImg = icons[icon] ? icons[icon] : GmailIcon;
+    return iconImg;
+}
+
+function Button({ colorBackground, colorText, text, onClick, icon }) {
     return (
-        <div>
-            <button className='o-button' 
-            style={{ 
+        icon == '' ?
+        <button className='o-button'
+            style={{
                 backgroundColor: GetColor(colorBackground),
                 color: GetColor(colorText),
-                borderColor: GetColor(colorBackground) }}>
-                {text}
-            </button>
-        </div>
+                borderColor: GetColor(colorBackground)
+            }}>
+            {text}
+        </button>
+        : 
+        <button className='o-button'
+            style={{
+                backgroundColor: GetColor(colorBackground),
+                color: GetColor(colorText),
+                borderColor: GetColor(colorBackground)
+            }}>
+            <img className='o-img-icon' src={GetIcon(icon)} />
+        </button>
     );
 }
 
