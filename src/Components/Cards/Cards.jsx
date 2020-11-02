cb584ba5f5e9417e70ada2573e2054b3e29db5b9
Kevin Marmolejo <kevin.marmolejo@uao.edu.co>
2020-11-02 16:11:37 -0500

    Cards

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,71 +1,23 @@
-import { CircularProgress } from "@material-ui/core";
-import Axios from "axios";
-import React, { useEffect } from "react";
-import Card from "./Card/Card";
+import React from 'react'
+import Card from './Card/Card';
+import './Cards.css'
 
-const operation = retry.operation({
-  retries: 5,
-  factor: 3,
-  minTimeout: 1 * 1000,
-  maxTimeout: 60 * 1000,
-  randomize: true,
-});
-
-function Cards({ nCards }) {
-  const [users, SetUsers] = React.useState([]);
-
-  useEffect(async () => {
-    //PARA PROBAR ESTO NECESITAS ACTIVAR ESA EXTENSION DE CORPS POLICY
-
-    //ES NECESARIO LLAMAR ESTA API "nCards" VECES PERO NO HE PODIDO.
-    //SE TIENE QUE LLENAR EL ARREGLO "users" PARA RETORNAR X CARDS CON SU INFORMACIÓN
-    for (let i = 0; i < nCards; i++) {
-      //Api para traer nombres e imagenes de personas aleatorias
-      Axios.get("https://randomuser.me/api/")
-        .then((response) => {
-          //Si se llama la API pero a veces falla entonces por ejemplo me trae 1 o 2 resultados, nosotros necesitamos
-          //"nCards" resultados, buscate como funcionan los retry en esta libreria Axios para volver a llamar la consulta cuando falle
-          var result = response.data;
-          var name =
-            result.results[0].name.first + " " + result.results[0].name.last;
-          var image = result.results[0].picture.large;
-          //Se crea un objeto usuario con un nombre y una imagen
-          var user = { name: name, image: image };
-          console.log(user);
-          //Se suma al arreglo de usuarios
-          SetUsers(users.push(user));
-          console.log(users.length);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
-  }, []);
-
-  if (users.length !== nCards) {
-    return <CircularProgress />;
-  } else {
-    return (
-      <div className="o-cards-container">
-        {console.log(users)}
-
-        {/* {users.map((user) => {
-        const rand = 0 + Math.random() * (5 - 0);
-        console.log("/////");
-        console.log(users.length);
-        return (
-          <Card
-            type={"Belleza"}
-            name={user.name}
-            profesion={"Estilista profesional"}
-            numStars={rand}
-            image={user.image}
-          />
-        );
-      })} */}
-      </div>
+function Cards (){
+    return(
+        <div className="o-cards-container">            
+            <Card type={"Belleza"} name={"Ana Elena"} profesion={"Estilista profesional"} image={"https://img.vixdata.io/pd/jpg-large/es/sites/default/files/imj/imujer/6/6-preguntas-que-debes-hacerle-a-tu-estilista-antes-de-que-comience-a-trabajar-1.jpg"} />
+            <Card type={"Belleza"} name={"Ana Elena"} profesion={"Estilista profesional"} image={"https://img.vixdata.io/pd/jpg-large/es/sites/default/files/imj/imujer/6/6-preguntas-que-debes-hacerle-a-tu-estilista-antes-de-que-comience-a-trabajar-1.jpg"} />
+            <Card type={"Belleza"} name={"Ana Elena"} profesion={"Estilista profesional"} image={"https://img.vixdata.io/pd/jpg-large/es/sites/default/files/imj/imujer/6/6-preguntas-que-debes-hacerle-a-tu-estilista-antes-de-que-comience-a-trabajar-1.jpg"} />
+            <Card type={"Belleza"} name={"Ana Elena"} profesion={"Estilista profesional"} image={"https://img.vixdata.io/pd/jpg-large/es/sites/default/files/imj/imujer/6/6-preguntas-que-debes-hacerle-a-tu-estilista-antes-de-que-comience-a-trabajar-1.jpg"} />
+            <Card type={"Belleza"} name={"Ana Elena"} profesion={"Estilista profesional"} image={"https://img.vixdata.io/pd/jpg-large/es/sites/default/files/imj/imujer/6/6-preguntas-que-debes-hacerle-a-tu-estilista-antes-de-que-comience-a-trabajar-1.jpg"} />
+            <Card type={"Belleza"} name={"Ana Elena"} profesion={"Estilista profesional"} image={"https://img.vixdata.io/pd/jpg-large/es/sites/default/files/imj/imujer/6/6-preguntas-que-debes-hacerle-a-tu-estilista-antes-de-que-comience-a-trabajar-1.jpg"} />
+            <Card type={"Belleza"} name={"Ana Elena"} profesion={"Estilista profesional"} image={"https://img.vixdata.io/pd/jpg-large/es/sites/default/files/imj/imujer/6/6-preguntas-que-debes-hacerle-a-tu-estilista-antes-de-que-comience-a-trabajar-1.jpg"} />
+            <Card type={"Belleza"} name={"Ana Elena"} profesion={"Estilista profesional"} image={"https://img.vixdata.io/pd/jpg-large/es/sites/default/files/imj/imujer/6/6-preguntas-que-debes-hacerle-a-tu-estilista-antes-de-que-comience-a-trabajar-1.jpg"} />
+            <Card type={"Belleza"} name={"Ana Elena"} profesion={"Estilista profesional"} image={"https://img.vixdata.io/pd/jpg-large/es/sites/default/files/imj/imujer/6/6-preguntas-que-debes-hacerle-a-tu-estilista-antes-de-que-comience-a-trabajar-1.jpg"} />
+            <Card type={"Belleza"} name={"Ana Elena"} profesion={"Estilista profesional"} image={"https://img.vixdata.io/pd/jpg-large/es/sites/default/files/imj/imujer/6/6-preguntas-que-debes-hacerle-a-tu-estilista-antes-de-que-comience-a-trabajar-1.jpg"} />
+            <Card type={"Belleza"} name={"Ana Elena"} profesion={"Estilista profesional"} image={"https://img.vixdata.io/pd/jpg-large/es/sites/default/files/imj/imujer/6/6-preguntas-que-debes-hacerle-a-tu-estilista-antes-de-que-comience-a-trabajar-1.jpg"} />
+        </div>
     );
-  }
 }
 
 export default Cards;
